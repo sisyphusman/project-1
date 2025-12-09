4fa2ce55d3120d0bcd4b53a39f91c390481265ad
Dohooyi <59619810+sisyphusman@users.noreply.github.com>
2025-12-09 19:12:08 +0900

    feat: SFML 창 띄우기 및 CMakeLists를 SFML 3에 맞게 수정

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "CMake Test" << std::endl;
+	// 1920x1080 창 생성
+	sf::RenderWindow window(sf::VideoMode({ 1920u, 1080u}), "project-1 with SFML");
+
+	// 메인 루프
+	while (window.isOpen())
+	{
+		// 이벤트 처리, pollEvent가 이벤트 반환
+		while (const std::optional<sf::Event> event = window.pollEvent())
+		{
+			// 창 닫기
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+
+			// 키 입력
+			if (event->is<sf::Event::KeyPressed>())
+			{
+				const auto* keyEvent = event->getIf<sf::Event::KeyPressed>();
+				if (keyEvent && keyEvent->code == sf::Keyboard::Key::Escape)
+				{
+					window.close();
+				}
+			}
+		}
+
+		// 랜더링
+		window.clear();
+		window.display();
+	}
+
 	return 0;
 }
