49943741b05e41808c767b03eb449c58040177a6
Dowoo <59619810+sisyphusman@users.noreply.github.com>
2026-02-04 09:56:13 +0900

    chore: 윈도우 대소문자 파일 이름 버그 발견

--- src/Engine/Entities/Player.cpp
+++
@@ -1,6 +0,0 @@
-#include "Engine/Entities/Player.h"
-
-Player::Player(int x, int y)
-	: Entity(x, y, '@')
-{
-}
--- src/Engine/Entities/Player.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Engine/Entities/Entity.h"
-
-class Player : public Entity
-{
-public:
-	Player(int x, int y);
-};
